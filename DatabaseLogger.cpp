bdef201eb0de4eab6c9a218186c5a12b67d949a6
Martin <martin@urbackup.org>
2024-03-24 17:26:36 +0100

    Fixup namespace

--- DatabaseLogger.cpp
+++ DatabaseLogger.cpp
@@ -8,36 +8,38 @@
 
 using namespace sqlgen;
 
+void DatabaseLogger::Log(const std::string& msg, LogLevel loglevel)
+{
+	const char* ll_str;
+	switch (loglevel)
+	{
+	case LL_DEBUG:
+		ll_str = "DEBUG: ";
+		break;
+	case LL_INFO:
+		ll_str = "INFO: ";
+		break;
+	case LL_WARNING:
+		ll_str = "WARNING: ";
+		break;
+	case LL_ERROR:
+		ll_str = "ERROR: ";
+		break;
+	default:
+		ll_str = "";
+		break;
+	}
+
+	std::cout << ll_str << msg << std::endl;
+}
+
 namespace sqlgen
 {
-	void DatabaseLogger::Log(const std::string& msg, LogLevel loglevel)
-	{
-		const char* ll_str;
-		switch (loglevel)
-		{
-		case LL_DEBUG:
-			ll_str = "DEBUG: ";
-			break;
-		case LL_INFO:
-			ll_str = "INFO: ";
-			break;
-		case LL_WARNING:
-			ll_str = "WARNING: ";
-			break;
-		case LL_ERROR:
-			ll_str = "ERROR: ";
-			break;
-		default:
-			ll_str = "";
-			break;
-		}
-
-		std::cout << ll_str << msg << std::endl;
-	}
 
 	IDatabaseLogger* getDatabaseLogger()
 	{
 		static DatabaseLogger logger;
 		return &logger;
 	}
+
 }
